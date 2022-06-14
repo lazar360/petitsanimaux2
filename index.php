ff2b09e40dc026edb293e854aed1b6a4d0b2b9c8
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-14 22:48:45 +0200

    update index

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
 require_once "controllers/frontend.controller.php";
+require_once "config/Securite.class.php";
 try{
 
     if (isset($_GET['page']) && !empty($_GET['page'])) {
-        switch ($_GET['page']) {
+        $page = Securite::secureHTML($_GET['page']); 
+        switch ($page) {
             case 'accueil': 
                 getPageAccueil();
                 break;
@@ -42,17 +44,24 @@ try{
                 getPageMentions();        
                 break;      
             case 'nouvelles':
-                getPageNouvelles();        
+                getPageActus();        
                 break;
             case 'animal':
                 getPageAnimal();        
                 break; 
-            case 'error404':
-                throw new Exception("Page introuvable Error 404");    
-                break; 
+            case 'error301':   
+            case 'error302':         
+            case 'error400':
+            case 'error402':
+            case 'error405':
+            case 'error500':
+            case 'error505':
+                throw new Exception("Erreur de type : ". $page);
+                break;
             case 'error403':
-                throw new Exception("Accès refusé Error 403");
+                throw new Exception("Accès refusé : Erreur 403");
                 break; 
+            case 'error404': 
             default:
                 throw new Exception("Page introuvable");
                 break;
