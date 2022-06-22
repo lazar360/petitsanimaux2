62be65fa0fc7eff5189002da6e6e285c6e905d35
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-22 19:15:00 +0200

    update actualite dao

--- models/actualite.dao.php
+++ models/actualite.dao.php
@@ -1,13 +1,17 @@
 <?php
 require_once("pdo.php");
 
-function getActualitesFromBD() {
+function getActualitesFromBD($type) {
     $bdd = connexionPDO();
     $req = 'SELECT * 
-            FROM actualite';
+            FROM actualite
+            WHERE type_actualite = :type  
+            ORDER BY date_publication_actualite DESC
+            ';
 
     
     $stmt = $bdd->prepare($req);
+    $stmt->bindValue(':type', $type, PDO::PARAM_STR);
     $stmt->execute();
     $actualites = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $stmt->closeCursor();
@@ -25,11 +29,56 @@ function getImageActualiteFromBD($idImage){
 
     $stmt = $bdd->prepare($req);
     $stmt->bindValue(':idImage', $idImage, PDO::PARAM_INT);
-    //$stmt->bindValue(":idStatut", $_GET["idstatut"]);
     $stmt->execute();
     $image = $stmt->fetch(PDO::FETCH_ASSOC);
     $stmt->closeCursor();
 
     return $image;
+
+}
+
+function getLastNews(){
+
+    $bdd = connexionPDO();
+    $req = 'SELECT id_actualite, libelle_actualite, contenu_actualite, date_publication_actualite, type_actualite, a.id_image, i.libelle_image, i.url_image, i.description_image 
+            FROM actualite a 
+            INNER JOIN image i ON a.id_image = i.id_image 
+            WHERE type_actualite = :type 
+            ORDER BY date_publication_actualite DESC 
+            LIMIT 1;
+            ';
+
+    
+    $stmt = $bdd->prepare($req);
+    $stmt->bindValue(':type', TYPE_NEWS, PDO::PARAM_STR);
+    $stmt->execute();
+    $actualite = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+   
+    return $actualite;
+
+}
+
+function getLastActionsOrEvents(){
+
+    $bdd = connexionPDO();
+    $req = 'SELECT id_actualite, libelle_actualite, contenu_actualite, date_publication_actualite, type_actualite, a.id_image, i.libelle_image, i.url_image, i.description_image 
+            FROM actualite a 
+            INNER JOIN image i ON a.id_image = i.id_image 
+            WHERE type_actualite = :typeEvent OR type_actualite = :typeAction
+            ORDER BY date_publication_actualite DESC
+            LIMIT 1;
+            ';
+
+    
+    $stmt = $bdd->prepare($req);
+    $stmt->bindValue(':typeEvent', TYPE_EVENTS, PDO::PARAM_STR);
+    $stmt->bindValue(':typeAction', TYPE_ACTIONS, PDO::PARAM_STR);
+    $stmt->execute();
+    $actualite = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+
+    return $actualite;
+
 
 }
