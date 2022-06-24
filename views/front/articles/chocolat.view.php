26b55cc89635d9b1b7ae41892a72da1fecc0f4bc
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-24 17:23:21 +0200

    update chocolat view

--- views/front/articles/chocolat.view.php
+++ views/front/articles/chocolat.view.php
@@ -7,7 +7,7 @@ echo(styleTitreNiveau1("Chocolat", COLOR_CONSEILS))
 
 <div class="row no-gutters">
     <div class="card col-auto mx-auto mt-2" style="width: 40rem;">
-        <img src="public/sources/images/Autres/Articles/Chocolat.jpg" class="card-img-top p-1" alt="Chocolat">
+        <img src="<?= URL ?>public/sources/images/sites/articles/Chocolat.jpg" class="card-img-top p-1" alt="Chocolat">
         <div class="card-body text-center">
             <?= (styleTitreNiveau2("Chocolat", COLOR_CONSEILS)) ?>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
