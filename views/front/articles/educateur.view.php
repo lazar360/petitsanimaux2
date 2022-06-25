96c0c96f39209b80f8680653c9a88165901e1f44
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-25 09:37:41 +0200

    update educateur view

--- views/front/articles/educateur.view.php
+++ views/front/articles/educateur.view.php
@@ -7,7 +7,7 @@ echo(styleTitreNiveau1("Educateur", COLOR_CONSEILS))
 
 <div class="row no-gutters">
     <div class="card col-auto mx-auto mt-2" style="width: 40rem;">
-        <img src="public/sources/images/Autres/Articles/imageEduc.jpg" class="card-img-top p-1" alt="Educateur">
+        <img src="<?= URL ?>public/sources/images/sites/articles/imageEduc.jpg" class="card-img-top p-1" alt="Educateur">
         <div class="card-body text-center">
             <?= (styleTitreNiveau2("Educateur", COLOR_CONSEILS)) ?>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
