217650c577983cff9399e4a9906fc43289c2bfa1
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-25 09:39:23 +0200

    update temperatures view

--- views/front/articles/temperatures.view.php
+++ views/front/articles/temperatures.view.php
@@ -7,7 +7,7 @@ echo(styleTitreNiveau1("Températures", COLOR_CONSEILS))
 
 <div class="row no-gutters">
     <div class="card col-auto mx-auto mt-2" style="width: 40rem;">
-        <img src="public/sources/images/Autres/Articles/Temperature.jpg" class="card-img-top p-1" alt="updp-logo">
+        <img src="<?= URL ?>public/sources/images/sites/articles/Temperature.jpg" class="card-img-top p-1" alt="updp-logo">
         <div class="card-body text-center">
             <?= (styleTitreNiveau2("Températures", COLOR_CONSEILS)) ?>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
