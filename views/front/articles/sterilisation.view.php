e375abad43e7b5f0ab57439790886228ff18553e
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-26 09:40:09 +0200

    update sterilisations view

--- views/front/articles/sterilisation.view.php
+++ views/front/articles/sterilisation.view.php
@@ -7,7 +7,7 @@ echo(styleTitreNiveau1("Stérilisation", COLOR_CONSEILS))
 
 <div class="row no-gutters">
     <div class="card col-auto mx-auto mt-2" style="width: 40rem;">
-        <img src="public/sources/images/Autres/Articles/Sterilisation.jpg" class="card-img-top p-1" alt="Sterilisation">
+        <img src="<?= URL ?>public/sources/images/sites/articles/Sterilisation.jpg" class="card-img-top p-1" alt="Sterilisation">
         <div class="card-body text-center">
             <?= (styleTitreNiveau2("Stérilisation", COLOR_CONSEILS)) ?>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
