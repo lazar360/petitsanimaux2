0ebfda994414c991b77a09c9956e461c7e2b572c
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-26 09:39:18 +0200

    update plantes view

--- views/front/articles/plantes.view.php
+++ views/front/articles/plantes.view.php
@@ -7,7 +7,7 @@ echo(styleTitreNiveau1("Plantes", COLOR_CONSEILS))
 
 <div class="row no-gutters">
     <div class="card col-auto mx-auto mt-2" style="width: 40rem;">
-        <img src="public/sources/images/Autres/Articles/Plantes toxiques.jpg" class="card-img-top p-1" alt="Plantes">
+        <img src="<?= URL ?>public/sources/images/sites/articles/Plantes toxiques.jpg" class="card-img-top p-1" alt="Plantes">
         <div class="card-body text-center">
             <?= (styleTitreNiveau2("Plantes", COLOR_CONSEILS)) ?>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
