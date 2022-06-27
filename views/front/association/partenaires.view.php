bc0758d2b3823c0430c0d18dd3ffacdeb30dd3ef
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-27 21:37:20 +0200

    update association view

--- views/front/association/partenaires.view.php
+++ views/front/association/partenaires.view.php
@@ -6,7 +6,7 @@ echo(styleTitreNiveau1("Les partenaires", COLOR_ASSO))
 
 <div class="row no-gutters">
     <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
-        <img src="public/sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
+        <img src="<?= URL ?>public/sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
         <div class="card-body text-center">
             <h5 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
