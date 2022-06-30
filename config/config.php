beb25751396cbb1f368646f49d6d3a39ed4c8ced
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-06-30 23:11:54 +0200

    update config

--- config/config.php
+++ config/config.php
@@ -15,5 +15,7 @@ const TYPE_NEWS = "News";
 const TYPE_ACTIONS = "Actions";
 const TYPE_EVENTS = "Event";
 
+const COOKIE_PROTECT = "timer";
+
 define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http"). "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
 ?>
