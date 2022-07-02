b7c2c9ee9bcada69ba937beabbaf390456ca9aec
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-07-03 00:09:25 +0200

    update admin dao

---
+++ models/admin.dao.php
@@ -0,0 +1,28 @@
+<?php
+require_once("pdo.php");
+
+function getPasswordUser($login){
+
+    $bdd = connexionPDO();
+    $req = '
+    SELECT * 
+    FROM administrateur 
+    where login = :login';
+    $stmt = $bdd->prepare($req);
+    $stmt->bindValue(":login",$login, PDO::PARAM_STR);
+    $stmt->execute();
+    $admin = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+    return $admin;
+
+}
+
+function isConnexionValid($login, $password){
+
+    $admin = getPasswordUser($login);
+    // echo ($admin); 
+    // echo($password);
+    // exit;
+    return password_verify($password, $admin['password']);
+
+}
