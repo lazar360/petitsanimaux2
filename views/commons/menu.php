e38aaaa7bfe06e62a555ffb6298b62ccabeb38c9
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-07-03 00:08:52 +0200

    update menu

--- views/commons/menu.php
+++ views/commons/menu.php
@@ -11,7 +11,7 @@ require_once"config/config.php";
     <ul class="navbar-nav mr-auto">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle perso_ColorRoseMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-          L'asso
+          Asso
         </a>
         <div class="dropdown-menu " aria-labelledby="navbarDropdown">
           <a class="dropdown-item perso_ColorRoseMenu" href="<?= URL ?>association">Qui sommes-nous ?</a>
@@ -26,6 +26,10 @@ require_once"config/config.php";
           <a class="dropdown-item perso_ColorOrangeMenu" href="<?= URL ?>pensionnaire&idstatut=<?=ID_STATUT_A_L_ADOPTION ?>">Ils cherchent une famille</a>
           <a class="dropdown-item perso_ColorOrangeMenu" href="<?= URL ?>pensionnaire&idstatut=<?=ID_STATUT_FALD ?>">Famille d'accueil longue durée</a>
           <a class="dropdown-item perso_ColorOrangeMenu" href="<?= URL ?>pensionnaire&idstatut=<?=ID_STATUT_ADOPTE ?>">Les anciens</a>
+          <?php if(isset($_SESSION['access']) && !empty($_SESSION['access'])) :?>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item perso_ColorVertMenu" href="<?= URL ?>genererPensionnaireAdmin">Gestion des pensionnaires</a>
+          <?php endif ?>
       </li>
 
       <li class="nav-item dropdown">
@@ -36,6 +40,12 @@ require_once"config/config.php";
         <a class="dropdown-item perso_ColorVertMenu" href="<?= URL ?>actus&type=<?= TYPE_NEWS ?>">Nouvelles des adoptés</a>
         <a class="dropdown-item perso_ColorVertMenu" href="<?= URL ?>actus&type=<?= TYPE_EVENTS ?>">Evènements</a>
         <a class="dropdown-item perso_ColorVertMenu" href="<?= URL ?>actus&type=<?= TYPE_ACTIONS ?>">Nos actions au quotidien</a>
+        
+        <?php if(isset($_SESSION['access']) && !empty($_SESSION['access'])) :?>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item perso_ColorOrangeMenu" href="<?= URL ?>genererNewsAdmin">Gestion des News</a>
+        <?php endif ?>
+
       </li>
 
       <li class="nav-item dropdown">
