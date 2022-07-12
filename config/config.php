72b6081e3c40a7c9e2b4041ca545635427b24657
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-07-12 06:23:53 +0200

    update config with array

--- config/config.php
+++ config/config.php
@@ -17,5 +17,10 @@ const TYPE_EVENTS = "Event";
 
 const COOKIE_PROTECT = "timer";
 
+const ALERT_SUCCESS = 1;
+const ALERT_DANGER = 2;
+const ALERT_INFO = 3;
+const ALERT_WARNING = 4;
+
 define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http"). "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
 ?>
