42a191f94e9421d2042ca3e572aee41f316c3b13
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-07-16 00:17:04 +0200

    update actualite dao

--- models/actualite.dao.php
+++ models/actualite.dao.php
@@ -13,7 +13,25 @@ function getActualitesFromBD($type) {
     $stmt = $bdd->prepare($req);
     $stmt->bindValue(':type', $type, PDO::PARAM_STR);
     $stmt->execute();
-    $actualites = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $actualites = $stmt->fetchAll(PDO::FETCH_ASSOC);//plusieurs datas donc fetch all 
+    $stmt->closeCursor();
+
+    return $actualites;
+}
+
+function getActualiteFromBD($idActualite) {
+    $bdd = connexionPDO();
+    $req = 'SELECT id_actualite, libelle_actualite, contenu_actualite, a.id_image, i.url_image, i.libelle_image, a.type_actualite
+            FROM actualite a
+            INNER JOIN image i on a.id_image = i.id_image
+            WHERE id_actualite = :idActualite  
+            ';
+
+    
+    $stmt = $bdd->prepare($req);
+    $stmt->bindValue(':idActualite', $idActualite, PDO::PARAM_INT);
+    $stmt->execute();
+    $actualites = $stmt->fetch(PDO::FETCH_ASSOC); //une seule data donc fetch simple 
     $stmt->closeCursor();
 
     return $actualites;
@@ -118,5 +136,23 @@ function insertActualiteIntoBD($titreActu, $typeActu, $contenuActu, $dateActu, $
     $stmt->closeCursor();
     if($resultat > 0) return true;
     else return false;
+
+}
+
+function insertImageNewsIntoBD ($nomImage, $url){
+
+    $bdd = connexionPDO();
+    $req = '
+    INSERT INTO image (libelle_image, url_image, description_image) 
+    VALUES (:nomImage, :url, :description);
+    ';
+    $stmt = $bdd->prepare($req);
+    $stmt->bindValue(':nomImage', $nomImage, PDO::PARAM_STR);
+    $stmt->bindValue(':url', $url, PDO::PARAM_STR);
+    $stmt->bindValue(':description', $nomImage, PDO::PARAM_STR);
+    $stmt->execute();
+    $resultat = $bdd->lastInsertId();
+    $stmt->closeCursor();
+    return $resultat;
 
 }
