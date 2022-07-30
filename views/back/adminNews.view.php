ac057991bf295a571a88f12137c00b7ba29790d3
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-07-30 19:33:34 +0200

    update adminNews view

--- views/back/adminNews.view.php
+++ views/back/adminNews.view.php
@@ -4,33 +4,16 @@ ob_start();
 echo (styleTitreNiveau1("Page de gestion des news", COLOR_ASSO))
 ?>
 
-<form action="" method="POST" enctype="multipart/form-data">
-    <div class="form-row">
-        <div class="form-group col-6">
-            <label for="titreActu">Titre de l'actualité :</label>
-            <input type="text" class="form-control" name="titreActu" id="titreActu" required>
-        </div>
-        <div class="form-group col-6">
-            <label for="typeActu">Titre de l'actualité :</label>
-            <select name="typeActu" id="typeActu" class="form-control">
-                <option value=""></option>
-            </select>
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="contenuActu">Titre de l'actualité :</label>
-        <textarea class="form-control" name="contenuActu" id="contenuActu" rows="5" required></textarea>
-    </div>
-    <div class="form-group">
-        <label for="imageActu">Image : </label>
-        <input type="file" class="form-control-file" name="imageActu" id="imageActu">
-    </div>
+<a href="genererNewsAdminAjout" class="btn btn-primary">Ajouter</a>
+<a href="genererNewsAdminModif" class="btn btn-primary">Modifier</a>
+<a href="genererNewsAdminSup" class="btn btn-primary">Supprimer</a>
 
-    <div class="row no-guters">
-        <input type="submit" value="Valider" class="btn btn-primary col">
-    </div>
-</form>
 
+<?= $contentAdminAction ?>
+
+<?php if($alert !==""){
+     echo afficherAlert($alert, $alertType);
+  } ?>
 
 <?php
 $content = ob_get_clean();
