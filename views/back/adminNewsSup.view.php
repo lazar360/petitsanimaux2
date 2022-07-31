de0592564db8c4c151cf5af750cbb259896997a1
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-07-31 17:24:46 +0200

    create views/back/adminNewsSup.view.php

---
+++ views/back/adminNewsSup.view.php
@@ -0,0 +1,15 @@
+<?php
+ob_start();
+
+echo (styleTitreNiveau2("Suppression d'une News", COLOR_ASSO))
+?>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    
+</form>
+
+
+<?php
+$contentAdminAction = ob_get_clean();
+
+?>
