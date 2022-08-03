e721e0671308e8ed4c35f33af6d11ae464d6194c
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-08-03 21:44:04 +0200

    create views/back/adminNewsAjout.view.php

---
+++ views/back/adminNewsAjout.view.php
@@ -0,0 +1,40 @@
+<?php
+ob_start();
+
+echo (styleTitreNiveau2("Ajout d'une News", COLOR_ASSO))
+?>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    <div class="form-row">
+        <div class="form-group col-6">
+            <label for="titreActu">Titre de l'actualité :</label>
+            <input type="text" class="form-control" name="titreActu" id="titreActu" required>
+        </div>
+        <div class="form-group col-6">
+            <label for="typeActu">Type d'actualité :</label>
+            <select name="typeActu" id="typeActu" class="form-control">
+            <?php foreach($typeActualites as $index => $type) : ?>    
+                <option value="<?= $type ?>"><?= $type ?></option>
+            <?php endforeach ?>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="contenuActu">Contenu de l'actualité :</label>
+        <textarea class="form-control" name="contenuActu" id="contenuActu" rows="5" required></textarea>
+    </div>
+    <div class="form-group">
+        <label for="imageActu">Image : </label>
+        <input type="file" class="form-control-file" name="imageActu" id="imageActu">
+    </div>
+
+    <div class="row no-guters">
+        <input type="submit" value="Valider" class="btn btn-primary col">
+    </div>
+</form>
+
+
+<?php
+$contentAdminAction = ob_get_clean();
+
+?>
