7393ea7628c5b55789075c35d5e41201233bff7f
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-08-12 19:34:56 +0200

    update views/back/adminNews.view.php

--- views/back/adminNews.view.php
+++ views/back/adminNews.view.php
@@ -6,14 +6,14 @@ echo (styleTitreNiveau1("Page de gestion des news", COLOR_ASSO))
 
 <a href="genererNewsAdminAjout" class="btn btn-primary">Ajouter</a>
 <a href="genererNewsAdminModif" class="btn btn-primary">Modifier</a>
-<a href="genererNewsAdminSup" class="btn btn-primary">Supprimer</a>
-
 
 <?= $contentAdminAction ?>
 
 <?php if($alert !==""){
      echo afficherAlert($alert, $alertType);
   } ?>
+
+<script src="public/js/verificationSuppressionActualite.js"></script>
 
 <?php
 $content = ob_get_clean();
